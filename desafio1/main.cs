3f0b7c0f40794fdc388e7bc29cc9dd7e436b9988
WILLIAM TAVARES <112444968+MestreWil@users.noreply.github.com>
2024-09-16 19:10:27 -0300

    Update main.cs

--- desafio1/main.cs
+++ desafio1/main.cs
@@ -1,14 +1,16 @@
+using ConsoleApp1;
 using System;
 
-public class Program
-	{
-		public static void Main(string[] args)
-        {
+class Program
+{
+    public static void Main(string[] args)
+    {
         Quadrado quadrado = new Quadrado(5.5);
         quadrado.VerArea();
         Circulo circulo = new Circulo(45);
         circulo.VerArea();
         Triangulo triangulo = new Triangulo(4, 5, 6);
         triangulo.VerArea();
-        }
-	}
+    }
+}
+
